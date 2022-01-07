8e3867b74a34b8d8d8ff9d021d16b9118a1e802d
t-94 <infourbh@gmail.com>
2022-01-07 15:19:03 +0900

    vue => tsx

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { defineComponent } from "vue";
+import HelloWorld from "./components/HelloWorld.vue";
+
+export default defineComponent({
+  name: "App",
+  components: {
+    HelloWorld
+  },
+  setup() {
+    return () => (
+      <>
+        <img alt="Vue logo" src="src/assets/logo.png" />
+        <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+      </>
+    )
+  }
+})
